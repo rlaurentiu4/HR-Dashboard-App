2a3337bee0ee4607abdd8467a7fa9e07b9db8f91
LaurentiuBadsi <50487193+LaurentiuBadsi@users.noreply.github.com>
2020-05-21 15:06:40 +0300

    icons for button

--- src/components/jobView.jsx
+++ src/components/jobView.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import JobTable from './jobTable.jsx';
 import AddAppForm from './addAppForm.jsx';
 
+import addApp from '../assets/icons/add_app.png';
+import apvApp from '../assets/icons/apv_app.png';
+import rmvApp from '../assets/icons/remove_app.png';
+import schApp from '../assets/icons/sch_app.png';
+
 class JobsView extends React.Component {
   constructor(props) {
     super(props);
@@ -29,12 +34,22 @@ class JobsView extends React.Component {
         <div>
           <div className="_view_btn-ctn">
             <button onClick={this.showFormApp} className="btn-action">
+              <img src={addApp} className="btn-icn" />
               Add Applicant
             </button>
             {showForm && <AddAppForm closeForm={this.closeFormApp} />}
-            <button className="btn-action">Approve Applicant</button>
-            <button className="btn-action">Remove Applicant</button>
-            <button className="btn-action">Schedule interview</button>
+            <button className="btn-action">
+              <img src={apvApp} className="btn-icn" />
+              Approve Applicant
+            </button>
+            <button className="btn-action">
+              <img src={rmvApp} className="btn-icn" />
+              Remove Applicant
+            </button>
+            <button className="btn-action">
+              <img src={schApp} className="btn-icn" />
+              Schedule interview
+            </button>
           </div>
           <div className="_view-table-ctn">
             <div style={{ display: 'flex' }}>
