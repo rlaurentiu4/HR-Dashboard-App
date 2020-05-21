632e70ee3c32084e3a8abdb061aa39d86460f1d5
LaurentiuBadsi <50487193+LaurentiuBadsi@users.noreply.github.com>
2020-05-21 15:46:56 +0300

    change for adding applicant
    btn for app moved to app view

--- src/components/applicatView.jsx
+++ src/components/applicatView.jsx
@@ -1,13 +1,44 @@
 import React from 'react';
 import AppTable from './appTable.jsx';
+import AddAppForm from './addAppForm.jsx';
+
+import addApp from '../assets/icons/add_app.png';
 
 class ApplicantView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false,
+    };
+
+    this.showFormApp = this.showFormApp.bind(this);
+    this.closeFormApp = this.closeFormApp.bind(this);
+  }
+
+  showFormApp() {
+    console.log('works');
+    this.setState({ showForm: true });
+  }
+
+  closeFormApp() {
+    this.setState({ showForm: false });
+  }
+
   render() {
+    const { showForm } = this.state;
     return (
       <React.Fragment>
-        <div className="_view-table-ctn">
-          <h1>App View</h1>
-          <AppTable />
+        <div>
+          <div className="_view_btn-ctn">
+            <button onClick={this.showFormApp} className="btn-action">
+              <img src={addApp} className="btn-icn" />
+              Add Applicant
+            </button>
+            {showForm && <AddAppForm closeForm={this.closeFormApp} />}
+          </div>
+          <div className="_view-table-ctn">
+            <AppTable />
+          </div>
         </div>
       </React.Fragment>
     );
--- src/components/jobView.jsx
+++ src/components/jobView.jsx
@@ -1,43 +1,16 @@
 import React from 'react';
 import JobTable from './jobTable.jsx';
-import AddAppForm from './addAppForm.jsx';
 
-import addApp from '../assets/icons/add_app.png';
 import apvApp from '../assets/icons/apv_app.png';
 import rmvApp from '../assets/icons/remove_app.png';
 import schApp from '../assets/icons/sch_app.png';
 
 class JobsView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showForm: false,
-    };
-
-    this.showFormApp = this.showFormApp.bind(this);
-    this.closeFormApp = this.closeFormApp.bind(this);
-  }
-
-  showFormApp() {
-    console.log('works');
-    this.setState({ showForm: true });
-  }
-
-  closeFormApp() {
-    this.setState({ showForm: false });
-  }
-
   render() {
-    const { showForm } = this.state;
     return (
       <React.Fragment>
         <div>
           <div className="_view_btn-ctn">
-            <button onClick={this.showFormApp} className="btn-action">
-              <img src={addApp} className="btn-icn" />
-              Add Applicant
-            </button>
-            {showForm && <AddAppForm closeForm={this.closeFormApp} />}
             <button className="btn-action">
               <img src={apvApp} className="btn-icn" />
               Approve Applicant
